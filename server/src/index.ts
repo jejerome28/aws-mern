cc4397ef32c5a712f0f7aa180d5b10366906e72f
jejerome <goyalajerome@gmail.com>
2023-03-02 17:06:45 +0800

    some changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('running on aws ec2');
 });
 
 app.listen(port, () => {
